dba959351841439fbf6280b13a26e3de4516f20b
andreaspap <andreaspap42@gmail.com>
2021-02-12 16:22:36 +0200

    data is ready for scikit

--- dist_params_selection.m
+++ dist_params_selection.m
@@ -5,10 +5,10 @@ save_folder_path = "D:\Work and uni\University\ML for food authentication\Result
 coeffs_title = "coefficients wt=db4levels=5.mat";
 coeffs_path=strcat(coeff_folder_path, coeffs_title);
 selection = [5 1;
-             4 3;
-             4 2;
-             3 2;
-             0 0
+             5 2;
+             5 3;
+             
+             0 0;
              ];
 distribution = 'Stable';
 dist_params=fit_distribution_selection(coeffs_path, save_folder_path, distribution, wavelet, levels, selection);
@@ -25,15 +25,16 @@ function dist_params = fit_distribution_selection(coeffs_path, save_folder_path,
             
             if level == 1 & aproximation_selected
                 dist = fitdist( coeffs{instance,1}{1, level}(1,:).', distribution);
-                dist_params{instance,1}{1, level} = dist.ParameterValues(1:2);
+                dist_params{instance,1}{1, level} = dist.ParameterValues(:).';
             else
                 dist_params{instance,1}{1, level} = [];
                 for orientation = 1:3
-                        [is_selected, Loc] = ismember([levels-level+2 orientation],selection,'rows');
-                        if is_selected 
-                            dist = fitdist( coeffs{instance,1}{1, level}(orientation,:).', distribution);
-                            dist_params{instance,1}{1, level}(orientation,:) = dist.ParameterValues(1:2);
-                        end
+                    [is_selected, Loc] = ismember([levels-level+2 orientation],selection,'rows');
+                    if is_selected 
+                        a=[level orientation]
+                        dist = fitdist( coeffs{instance,1}{1, level}(orientation,:).', distribution);
+                        dist_params{instance,1}{1, level}(orientation,:) = dist.ParameterValues(:).';
+                    end
                 end 
             end
         end
